c4e9cc735f17e14df05a5cab6214f00258fc839b
Mars Cheung <mars.kwong.cheung@gmail.com>
2026-01-08 16:28:03 -0800

    adjust project pullback to include eager loading of tasks

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -13,7 +13,7 @@ class ProjectController extends Controller
     public function index(Request $request)
     {
         $perPage = min($request->integer('page_size', 10), 100);
-        $projects = Project::where('user_id', '=', $request->user()->id)->paginate($perPage);
+        $projects = Project::where('user_id', '=', $request->user()->id)->with('task')->paginate($perPage);
         return response()->json([
             'data' => $projects
         ], 200);
